1ecf2dc156dc511272a901df7a0c0b28c2ea2b0c
NoahGX <114901822+NoahGX@users.noreply.github.com>
2022-10-30 00:05:57 -0700

    Prefab

--- AngryBird2D/Assets/land monster sprite sheets/Transparent PNG/idle/Enemy.cs
+++ AngryBird2D/Assets/land monster sprite sheets/Transparent PNG/idle/Enemy.cs
@@ -1,12 +1,16 @@
+using System.Numerics;
 using UnityEngine;
 
 public class Enemy : MonoBehaviour
 {
+    [SerializeField] private GameObject _cloudParticlePrefab;
+
     private void OnCollisionEnter2D(Collision2D collision)
     {
         AngryBird bird = collision.collider.GetComponent<AngryBird>();
         if (bird != null)
         {
+            Instantiate(_cloudParticlePrefab, transform.position, Quaternion.identity);
             Destroy(gameObject);
             return;
         }
@@ -19,6 +23,7 @@ public class Enemy : MonoBehaviour
 
         if (collision.contacts[0].normal.y < 0)
         {
+            Instantiate(_cloudParticlePrefab, transform.position, Quaternion.identity);
             Destroy(gameObject);
         }
     }
