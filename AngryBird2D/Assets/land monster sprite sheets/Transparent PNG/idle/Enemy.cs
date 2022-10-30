f16a8879f90358d277bac6bf024bf3f56b7e5b89
NoahGX <114901822+NoahGX@users.noreply.github.com>
2022-10-29 23:56:49 -0700

    Enemy Script

---
+++ AngryBird2D/Assets/land monster sprite sheets/Transparent PNG/idle/Enemy.cs
@@ -0,0 +1,27 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Enemy : MonoBehaviour
+{
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        AngryBird bird = collision.collider.GetComponent<Enemy>();
+        if (bird ! = null)
+        {
+            Destroy(gameObject);
+            return;
+        }
+
+        Enemy enemy = collision.collider.GetComponent<Enemy>();
+        if (enemy != null)
+        {
+            return;
+        }
+
+        if (collision.contacts[0].normal.y < 0)
+        {
+            Destroy(gameObject);
+        }
+    }
+}
