1f01be159b5bbe4700637e77b69da2b2da04e98d
NoahGX <114901822+NoahGX@users.noreply.github.com>
2022-10-31 13:30:17 -0700

    LevelController

--- AngryBird2D/Assets/Flappy Fat Bird Sprite Sheets/PNG/LevelController.cs
+++ AngryBird2D/Assets/Flappy Fat Bird Sprite Sheets/PNG/LevelController.cs
@@ -1,18 +1,26 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 
 public class LevelController : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    private static int _nextLevelIndex = 1;
+    private Enemy[] _enemies;
+
+    private void OnEnable()
     {
-        
+        _enemies = FindObjectsOfType<Enemy>();
     }
 
-    // Update is called once per frame
     void Update()
     {
-        
+        foreach(Enemy enemy in _enemies)
+        {
+            if (enemy != null)
+                return;
+        }
+
+        Debug.Log("You killed all enemies");
+
+        _nextLevelIndex++;
+        string nextLevelName = "Level" + _nextLevelIndex;
     }
 }
