c2a9a3956f67634cd9314f8c192efe8e48fd3151
NoahGX <114901822+NoahGX@users.noreply.github.com>
2022-10-30 20:35:50 -0700

    Select and Drag

---
+++ AngryBird2D/Assets/Flappy Fat Bird Sprite Sheets/PNG/AngryBird.cs
@@ -0,0 +1,22 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AngryBird : MonoBehaviour
+{
+    private void OnMouseDown()
+    {
+        GetComponent<SpriteRenderer>().color = Color.red;
+    }
+
+    private void OnMouseUp()
+    {
+        GetComponent<SpriteRenderer>().colo = Color.white;
+    }
+
+    private void OnMouseDrag()
+    {
+        Vector3 newPosition = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+        transform.position = new Vector3(newPosition.x, newPosition.y);
+    }
+}
